c6ea3410a5b5164d734ab8af016ac1b3b6dae304
Koro0 <ledane92@gmail.com>
2022-07-27 12:01:06 +0200

    add userCtrl with signUp & logIn

---
+++ controllers/user.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+/////////////////////////////////////creer nouveau compte utilisateur/////////////////////////////////
+exports.signUp = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const user = new User({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: 'Utilisateur créé!' });
+  } catch (error) {
+    res.status(400).json({ error: '!' });
+  }
+};
+
+///////////////////////////////////Connexion /////////////////////////////////////////////
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur inconnu' });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: 'Mot de passe incorrecte!' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+              expiresIn: '24h',
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error: 'error bcrypt' }));
+    })
+    .catch((error) => res.status(500).json({ error: 'err login' }));
+};
