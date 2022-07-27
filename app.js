6e39da1eadbd94505746c6e96fff01046605ec7c
Koro0 <ledane92@gmail.com>
2022-07-27 13:58:26 +0200

    add call login & signup

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+///////////////////// Appel route User /////////////////////////////////////////////////////////////////
+const userRoutes = require('./routes/user');
+
 mongoose
   .connect(
     'mongodb+srv://Koroo:xzeKMg1TUehmAvgR@cluster0.4yqb4.mongodb.net/test?retryWrites=true&w=majority',
@@ -9,6 +13,21 @@ mongoose
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+///// authentification signUp / logIn////
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
