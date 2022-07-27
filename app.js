7825ad6ca80c6364989db4f80c6eb056df7c1c00
Koro0 <ledane92@gmail.com>
2022-07-27 11:52:02 +0200

    add mongoose.connect

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://Koroo:xzeKMg1TUehmAvgR@cluster0.4yqb4.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
